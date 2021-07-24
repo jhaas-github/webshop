050f2c0307489cc426c13394823d046e8673fa45
Joey-Haas <jhaas.github@gmail.com>
2021-07-24 16:06:09 +0200

    Overall styling changes and burger menu.
    Functionality and styling of the burger menu aswell as overall styling of the menu.

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+// Open burger menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+// Close burger menu
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+//   Openen accordion menu in navigatie
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+} 
